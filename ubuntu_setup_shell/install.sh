db93589b65a5ca502ef1b3340e39911b702c8526
dybangel <176682769@qq.com>
2018-05-07 05:17:44 -0700

    添加系统安装初始华脚本

---
+++ ubuntu_setup_shell/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+aptpack="openssh-server lrzsz sox vim vim-scripts vim-doc ctags git"
+
+for i in $aptpack
+do
+	#have=`dpkg -l $i|wc -l`
+	#if [ $have -eq 0 ];then
+		apt install $i -y
+	#else
+	#	echo "$i already install don't install"
+	#fi
+
+done
+
+cp ./.vimrc ~/
